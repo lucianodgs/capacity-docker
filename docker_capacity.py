e77fdfbb1cdcbd4b9a402e2e3ea14d0989e04ca8
LUCIANO DINIZ GUERRA SANTIAGO <lucianodgs@gmail.com>
2017-10-29 21:43:07 -0200

    alteração de nomes de funções

--- docker_capacity.py
+++ docker_capacity.py
@@ -5,11 +5,11 @@ import docker
 
 def connectar():
     #client = docker.from_env()
-    #client = docker.DockerClient(base_url='tcp://192.168.187.135:2375')
+    #client = docker.DockerClient(base_url='tcp://192.168.187.135:15044')
     client = docker.DockerClient(base_url='unix://var/run/docker.sock')  
     return(client)  
 
-def listar():
+def listar_todos():
     client = connectar()
     get_all = client.containers.list(all)
 
@@ -17,12 +17,12 @@ def listar():
         conectando = client.containers.get(cada_conteiner.id)
         print("Container: %s imagem: %s comando: %s status %s"%(conectando.short_id, conectando.attrs['Config']['Image'],conectando.attrs['Config']['Cmd'],conectando.attrs['State']['Status']))
 
-def criar(imagem,comando):
+def criar_container(imagem,comando):
     client = connectar()
     executando = client.containers.run(imagem,comando)
     print(executando)
 
-def procurar(busca):
+def localizar(busca):
     client = connectar()
     get_all = client.containers.list(all)
     for cada_container in get_all:
@@ -35,4 +35,4 @@ def procurar(busca):
 
 
 #procurar("hello")        
-listar()
+listar_todos()
