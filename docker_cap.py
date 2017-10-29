81a56d80e498d115757a266cee391851b2331460
LUCIANO DINIZ GUERRA SANTIAGO <lucianodgs@gmail.com>
2017-10-29 21:30:39 -0200

    atualização para conexão remota

--- docker_cap.py
+++ docker_cap.py
@@ -3,19 +3,36 @@
 import subprocess
 import docker 
 
+def connectar():
+    #client = docker.from_env()
+    #client = docker.DockerClient(base_url='tcp://192.168.187.135:2375')
+    client = docker.DockerClient(base_url='unix://var/run/docker.sock')  
+    return(client)  
+
 def listar():
-    client = docker.from_env()
+    client = connectar()
     get_all = client.containers.list(all)
 
     for cada_conteiner in get_all:
         conectando = client.containers.get(cada_conteiner.id)
-        print("Container: %s imagem: %s comando: %s"%(conectando.short_id, conectando.attrs['Config']['Image'],conectando.attrs['Config']['Cmd']))
+        print("Container: %s imagem: %s comando: %s status %s"%(conectando.short_id, conectando.attrs['Config']['Image'],conectando.attrs['Config']['Cmd'],conectando.attrs['State']['Status']))
 
 def criar(imagem,comando):
-    client = docker.from_env()
+    client = connectar()
     executando = client.containers.run(imagem,comando)
     print(executando)
 
-#def procurar()
+def procurar(busca):
+    client = connectar()
+    get_all = client.containers.list(all)
+    for cada_container in get_all:
+        conectando = client.containers.get(cada_container.id)
+        imagem_container = conectando.attrs['Config']['Image']
+        if str(busca).lower() in str(imagem_container).lower():
 
-listar()        
+            print("Conteiner: %s |Busca: %s |nome:%s" %(cada_container.short_id,busca, imagem_container))
+
+
+
+#procurar("hello")        
+listar()
